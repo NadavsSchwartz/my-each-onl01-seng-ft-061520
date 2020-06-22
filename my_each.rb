9f6f6821fa0dfe7af00f27ee00b0896f176a8699
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-22 03:48:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
 def my_each(array)
-yield
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+  end
+end
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
